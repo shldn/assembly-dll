419606e643f4e4bca557e5f8a3497c5e0c7789a8
Erik Hill <eghill@ucsd.edu>
2017-09-06 12:00:05 -0700

    Initial commit

---
+++ Assembly/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+namespace Assembly
+{
+    public class Test
+    {
+        public static string Hello() {
+            Vector3 v1 = new Vector3(1,1,1);
+            Vector3 v2 = new Vector3(2,2,2);
+            return "Hello World " + (v1 + v2);
+        }
+
+        public static Vector3 Add(Vector3 v1, Vector3 v2) {
+            return v1 + v2;
+        }
+    }
+}
