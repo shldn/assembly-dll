e543ab08c69017ed93a5f61a7d134a50eb2ec723
Erik Hill <eghill@ucsd.edu>
2017-09-22 14:10:38 -0700

    Put colony update step on a separate thread. Added a sleep to the update step. Added a way to kill the colony.

--- Assembly/Colony.cs
+++ Assembly/Colony.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Numerics;
+using System.Threading;
 
 namespace Assembly
 {
@@ -9,25 +10,64 @@ namespace Assembly
         float minBoundary = -10;
         float maxBoundary = 10;
         List<Assembly> assemblies = new List<Assembly>();
+
+        // simulation control
+        static bool killAll = false;
+        bool running = true;
+
+        // frame rate control
+        int sleepMillis = 10;
+
+        // locks
+        Object transformDataLock = new object();
+
+        // helpers
         Random random = new Random();
 
-        public Colony(int num) {
-            
+        public static Colony Create(int num) {
+            // Create the new Colony
+            Colony colony = new Colony(num);
+
+            // Start the update loop in the new thread.
+            Thread t = new Thread(new ThreadStart(colony.UpdateLoop));
+            t.Start();
+
+            return colony;
+        }
+
+        public static void KillAll() {
+            killAll = true;
+        }
+
+        private Colony(int num) {
+            Console.WriteLine("Creating a colony " + num);
             for (int i = 0; i < num; ++i) {
                 assemblies.Add(new Assembly(RandomPos()));
             }
         }
+
+        private void UpdateLoop() {
+            while (running && !killAll) {
+                Update();
+                Thread.Sleep(sleepMillis);
+            }
+        }
+
         // This should be internal, but exposing for now to test
         public void Update() {
-            for (int i = 0; i < assemblies.Count; ++i) {
-                assemblies[i].position += 0.001f * assemblies[i].position;
+            lock (transformDataLock) {
+                for (int i = 0; i < assemblies.Count; ++i) {
+                    assemblies[i].position += 0.001f * assemblies[i].position;
+                }
             }
         }
 
         public List<Vector3> GetPositions() {
             List<Vector3> positions = new List<Vector3>();
-            for(int i = 0; i < assemblies.Count; ++i) {
-                positions.Add(assemblies[i].position);
+            lock (transformDataLock) {
+                for (int i = 0; i < assemblies.Count; ++i) {
+                    positions.Add(assemblies[i].position);
+                }
             }
             return positions;
         }
